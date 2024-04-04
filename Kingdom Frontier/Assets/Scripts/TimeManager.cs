49413da7b14b273c2b305b3a374c48a00ad26914
MEHMET YILDIZ <info@bymehmetyildiz.com>
2024-04-04 15:47:57 +0300

    DayCycle

---
+++ Kingdom Frontier/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public Gradient ambianColor;
+    public Gradient fogColor;
+    public Gradient directionalLightColor;
+
+    public Light directionalLight;
+
+    [Range(0, 24)] public float dayTime;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        dayTime += Time.deltaTime/5;
+        dayTime %= 24;
+
+        UpdateLight(dayTime / 24);
+    }
+
+    private void UpdateLight(float timePercent)
+    {
+        RenderSettings.ambientLight = ambianColor.Evaluate(timePercent);
+        RenderSettings.fogColor = fogColor.Evaluate(timePercent);
+
+        directionalLight.color = directionalLightColor.Evaluate(timePercent);
+        directionalLight.transform.localRotation = Quaternion.Euler((timePercent * 360) - 90, 270, 0);
+    }
+}
